efffd6c060e12b5ef15c27750ee94462ba789fd8
Aniyan <jacob.aniyan@kpmg.co.uk>
2026-01-23 12:01:34 +0000

    Seeded development and test database, set up testing

---
+++ RecipeGenerator.test/Recipes.cs
@@ -0,0 +1,30 @@
+﻿using RecipeGenerator.Data;
+using RecipeGenerator.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.test
+{
+  
+    
+        public class Recipes : IClassFixture<CustomWebApplicationFactory>
+        {
+            private readonly CustomWebApplicationFactory _factory;
+
+            public Recipes(CustomWebApplicationFactory factory)
+            {
+                _factory = factory;
+            }
+
+            [Fact]
+            public void Database_Should_Be_Created()
+            {
+
+            }
+        }
+ }
+
+
