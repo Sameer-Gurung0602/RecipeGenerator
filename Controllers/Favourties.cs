b1970516a5aec622f586a500cb307038edf69bf8
Gurung, Sameer <Sameer.Gurung@kpmg.co.uk>
2026-01-27 14:05:18 +0000

    created the savedrecipes controller

---
+++ Controllers/Favourties.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Favourites : Controller
+    {
+    
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetSavedRecipes()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult SaveRecipe(int  id)
+        {
+
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveRecipe(int id)
+        {
+            return Ok(); }
+
+        }
+}
