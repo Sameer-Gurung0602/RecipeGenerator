bdbe20432db3616fb4c8d4d403ec13fc03c04a4c
Gurung, Sameer <Sameer.Gurung@kpmg.co.uk>
2026-01-27 09:51:14 +0000

    added recipeMatchDto

--- DTOs/RecipeMatchDto.cs
+++ DTOs/RecipeMatchDto.cs
@@ -2,5 +2,18 @@
 {
     public class RecipeMatchDto
     {
+        public int RecipeId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int CookTime { get; set; }
+        public string Difficulty { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        public int MatchPercentage { get; set; }
+        public int TotalIngredientsRequired { get; set; }
+        public int IngredientsMatched { get; set; }
+        public List <string> MatchedIngredients { get; set; } = new();
+        public List<string> MissingIngredients { get; set; } = new();
+
     }
 }
