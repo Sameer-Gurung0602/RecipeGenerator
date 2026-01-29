c96b4617022e04fcee6f4cdb6257947458428617
Gurung, Sameer <Sameer.Gurung@kpmg.co.uk>
2026-01-29 15:12:52 +0000

    sorted out duplicate favouritescontrollers

--- Controllers/FavouritesController.cs
+++ Controllers/FavouritesController.cs
@@ -38,5 +38,17 @@ namespace RecipeGenerator.Controllers
 
             return Ok("Recipe saved to favourites successfully.");
         }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveRecipe(int id)
+        {
+            var success = await _favouritesService.RemoveFavourite(id);
+
+            if (!success)
+            {
+                return NotFound(new { message = $"Recipe with ID {id} not found or not in favourites." });
+            }
+
+            return Ok(new { message = "Recipe removed from favourites successfully" });
+        }
     }
 }
